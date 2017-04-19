3e48af2c218187cf1d6ed406d03cc14f94ecb991
zhchang <zhchang@gmail.com>
2017-04-19 18:32:01 +0800

    remove docker configs

--- build.sh
+++
@@ -1,4 +0,0 @@
-set -e
-docker build -t gotour .
-docker tag gotour:latest zhchang/gotour:latest
-docker push zhchang/gotour:latest
--- run.sh
+++
@@ -1,3 +0,0 @@
-docker stop gotour
-docker rm gotour
-docker run --name gotour -t gotour
