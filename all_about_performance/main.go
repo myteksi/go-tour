4545e32b1d1ecf161c18a907b2c41c91d2d3b668
zhchang <zhchang@gmail.com>
2017-06-07 12:32:44 +0800

    makesure abp compiles

--- all_about_performance/main.go
+++ all_about_performance/main.go
@@ -3,4 +3,5 @@ package main
 // format takes 2 arguments and output a formatted string
 // sample: format(1,2.0) => "formated-int(1)-float(2.00)" the float number shall always have 2 digits after decimal point.
 func format(intValue int64, floatValue float64) string {
+	return ""
 }
