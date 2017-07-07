e1644c5f37a6bfedbcd637e209aa92a21844093f
yonghao.chen <yonghao.chen@grabtaxi.com>
2017-07-07 17:15:04 +0800

    removed confusing code

--- init_trap/api/api.go
+++ init_trap/api/api.go
@@ -1,11 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"time"
-
-	"errors"
-
 	"github.com/myteksi/go-tour/init_trap/config"
 )
 
@@ -26,21 +21,4 @@ func init() {
 	}
 
 	timeoutInMs = config.Config.ApiConf.TimeoutInMs
-}
-
-//LoadUsernameByID loads username by ID
-func LoadUsernameByID(ID int64) (string, error) {
-	nameChan := make(chan string, 1)
-	go func(ID int64, c chan string) {
-		time.Sleep(500 * time.Millisecond)
-		username := fmt.Sprintf("username:%d", ID)
-		c <- username
-	}(ID, nameChan)
-
-	select {
-	case name := <-nameChan:
-		return name, nil
-	case <-time.After(time.Duration(timeoutInMs) * time.Millisecond):
-		return "", errors.New("request timeout")
-	}
 }
--- init_trap/main_test.go
+++ init_trap/main_test.go
@@ -3,28 +3,14 @@ package init
 import (
 	"testing"
 
+	"github.com/myteksi/go-tour/init_trap/api"
+	"github.com/myteksi/go-tour/init_trap/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUseApi(t *testing.T) {
-	tests := []struct {
-		msg              string
-		ID               int64
-		expectedUsername string
-		expectedErr      error
-	}{
-		{
-			msg:              "happy test",
-			ID:               1,
-			expectedUsername: "username:1",
-			expectedErr:      nil,
-		},
-	}
+	// init config
+	config.Init()
 
-	for _, test := range tests {
-		username, err := useAPI(test.ID)
-		assert.Equal(t, test.expectedUsername, username, test.msg)
-		assert.Equal(t, test.expectedErr, err)
-
-	}
+	assert.True(t, api.IsEnabled())
 }
